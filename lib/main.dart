ba6e55f54a74f4b41a17e9043b4a7122238bc0f9
SMART24\User <fds44fs@nate.com>
2025-05-19 12:40:48 +0900

    password

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:chungju_lecture/todo_local_page.dart';
+import 'package:chungju_lecture/todo_remote.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'home_page.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ✅ .env 파일 로딩
+  await dotenv.load(fileName: '.env'); // 기본: .env
+
+  // ✅ Supabase 초기화
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
 }
 
@@ -20,7 +35,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         '/': (context) => const HomePage(),
-        '/todo-local': (context) => const TodoLocalPage()
+        '/todo-local': (context) => const TodoLocalPage(),
+        '/todo-remote': (context) => const TodoRemotePage()
       },
     );
   }
