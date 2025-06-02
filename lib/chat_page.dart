c181b9922630cdaa3c55802aa5b7520150e9ab0a
SMART24\User <fds44fs@nate.com>
2025-06-02 11:13:10 +0900

    update

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -8,8 +9,100 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final supabase = Supabase.instance.client;
+  final List<Map<String, dynamic>> _messages = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _loadMessage() async {
+    final data = await supabase
+                  .from("messages")
+                  .select()
+                  .order('created_at', ascending: false);
+    setState(() {
+      _messages.clear();
+      _messages.addAll(List<Map<String, dynamic>>.from(data));
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadMessage();
+    _subscribeToMessage();
+  }
+
+  void _subscribeToMessage() {
+    supabase
+      .channel('public:messages')
+        .onPostgresChanges(
+          event: PostgresChangeEvent.insert,
+          schema: 'public',
+          table: 'messages',
+          callback: (payload) {
+            setState(() {
+              _messages.insert(0, payload.newRecord);
+            });
+          }
+        ).subscribe();
+  }
+
+  void _sendMesage() async {
+    final text = _controller.text.trim();
+    final user = supabase.auth.currentUser;
+    if(text.isNotEmpty && user != null){
+      await supabase.from("messages").insert(
+        {
+          'user_id' : user.id,
+          'content' : text
+        }
+      );
+      _controller.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("채팅")),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                reverse: true,
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  final msg = _messages[index];
+                  return ListTile(
+                    title: Text(msg['content'] ?? ''),
+                  );
+                }
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    onSubmitted: (_) => _sendMesage(),
+                    decoration: const InputDecoration(
+                      hintText: "메세지 입력...",
+                      border: OutlineInputBorder()
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8,),
+                IconButton(
+                    onPressed: _sendMesage,
+                    icon: const Icon(Icons.send)
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
