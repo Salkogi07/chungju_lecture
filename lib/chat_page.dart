b1b18d498fa701a428175fa0c90b67e7ca45a88b
SMART24\User <fds44fs@nate.com>
2025-06-02 12:40:26 +0900

    update

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -54,7 +54,8 @@ class _ChatPageState extends State<ChatPage> {
       await supabase.from("messages").insert(
         {
           'user_id' : user.id,
-          'content' : text
+          'content' : text,
+          'email' : user.email
         }
       );
       _controller.clear();
@@ -73,8 +74,24 @@ class _ChatPageState extends State<ChatPage> {
                 itemCount: _messages.length,
                 itemBuilder: (context, index) {
                   final msg = _messages[index];
+                  final userId = supabase.auth.currentUser?.id;
+                  final email = msg['email'] ?? "";
+                  final isMine = msg['user_id'] == userId;
+                  final senderText = isMine ? "나" : email;
+
                   return ListTile(
-                    title: Text(msg['content'] ?? ''),
+                    title: Text(
+                        msg['content'] ?? '',
+                        textAlign: isMine ? TextAlign.end : TextAlign.start,
+                    ),
+                    subtitle: Text(
+                        senderText,
+                        textAlign: isMine ? TextAlign.end : TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey[600]
+                        ),
+                    ),
                   );
                 }
             ),
